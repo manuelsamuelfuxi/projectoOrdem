4c6ef4919f509fbf9e24e17f5128deba681b48b2
manuelsamuelfuxi <157748782+manuelsamuelfuxi@users.noreply.github.com>
2026-06-20 08:47:55 +0100

    tirei o erro do envio do formulario

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,24 +7,19 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Como estamos a usar Virtual Host no XAMPP (ordepdita.test),
-        // forçamos o Laravel a gerar as URLs usando esse domínio.
-        // Isso garante que links de email, assets e metadados estejam correctos.
         if (app()->environment('local')) {
             URL::forceRootUrl('http://ordepdita.test');
+        }
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
